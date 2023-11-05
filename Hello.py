214c86dd57cd85290df48ae0096f2604e003e12d
valeriebussing <84744308+valeriebussing@users.noreply.github.com>
2023-11-05 16:42:13 +0000

    Created using Colaboratory

---
+++ Hello.py
@@ -0,0 +1,30 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyN9mkpZ2MSW8pklfH0K4mJ2"
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "KJPaFtdJWwz5"
+      },
+      "outputs": [],
+      "source": [
+        "print(\"Hello world!\")"
+      ]
+    }
+  ]
+}
